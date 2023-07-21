0b953e3515a7a02cfc2a352bbe0cde88f0937bce
Pethum Jeewantha <pethumjeewantha1@gmail.com>
2023-07-21 22:43:29 +0530

    main

--- src/com/bitzify/Main.java
+++ src/com/bitzify/Main.java
@@ -2,7 +2,7 @@ package com.bitzify;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world! Main");
     }
 }
 
